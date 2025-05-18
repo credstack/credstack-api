4dd5bc140a16ed96ee5da04356baefb2d9307468
Steven A. Zaluk <stevezaluk@gmail.com>
2025-05-18 12:12:43 -0400

    Cobra generated code for basic CLI

---
+++ cmd/root.go
@@ -0,0 +1,54 @@
+/*
+Copyright © 2025 Steven A. Zaluk
+*/
+
+package cmd
+
+import (
+	"fmt"
+	"os"
+
+	"github.com/spf13/cobra"
+	"github.com/spf13/viper"
+)
+
+var cfgFile string
+
+var rootCmd = &cobra.Command{
+	Use:   "credstack-api",
+	Short: "",
+	Long:  `RESTful API for CredStack IDP`,
+}
+
+func Execute() {
+	err := rootCmd.Execute()
+	if err != nil {
+		os.Exit(1)
+	}
+}
+
+func init() {
+	cobra.OnInitialize(initConfig)
+
+	rootCmd.PersistentFlags().StringVar(&cfgFile, "config", "", "config file (default is $HOME/.credstack/config.json)")
+}
+
+func initConfig() {
+	if cfgFile != "" {
+		viper.SetConfigFile(cfgFile)
+	} else {
+		home, err := os.UserHomeDir()
+		cobra.CheckErr(err)
+
+		viper.AddConfigPath(home + "/.credstack")
+		viper.SetConfigType("json")
+		viper.SetConfigName("config.json")
+	}
+
+	viper.AutomaticEnv()
+
+	if err := viper.ReadInConfig(); err != nil {
+		fmt.Println("Failed to read config file: ", err.Error())
+		os.Exit(1)
+	}
+}
