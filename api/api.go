8b1a4c4673644dc0f37a483b5ff360717debc74f
Steven A. Zaluk <stevezaluk@gmail.com>
2025-05-18 14:39:39 -0400

    Added server and app fields to the API

--- api/api.go
+++ api/api.go
@@ -1,15 +1,30 @@
 package api
 
+import (
+	"github.com/gofiber/fiber/v3"
+	"github.com/stevezaluk/credstack-lib/server"
+)
+
 /*
 API - Provides a core abstraction of the API itself. Primarily handles dependency injection and graceful termination
 of the API
 */
 type API struct {
+	// server - The server structure that provides dependencies for the API
+	server *server.Server
+
+	// app - The primary fiber application that handles API level routing
+	app *fiber.App
 }
 
 /*
-New - A constructor for the API
+New - A constructor for the API. This will use the server.Server structure passed in its parameter, and initialize a
+new fiber.App for you. Note: The server structure provided here should not have a pre-connected database, this structure
+will handle server.Database lifecycle for you
 */
-func New() *API {
-	return &API{}
+func New(server *server.Server) *API {
+	return &API{
+		server: server,
+		app:    fiber.New(),
+	}
 }
