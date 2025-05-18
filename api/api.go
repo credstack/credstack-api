006283329d9e5cae9221f40bd3c885520e35fcd4
Steven A. Zaluk <stevezaluk@gmail.com>
2025-05-18 14:34:55 -0400

    Defined a basic structure for the API along with a simple constructor

--- api/api.go
+++ api/api.go
@@ -1,3 +1,15 @@
 package api
 
+/*
+API - Provides a core abstraction of the API itself. Primarily handles dependency injection and graceful termination
+of the API
+*/
+type API struct {
+}
 
+/*
+New - A constructor for the API
+*/
+func New() *API {
+	return &API{}
+}
