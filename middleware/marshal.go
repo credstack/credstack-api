fcda6cc82656c9824fb5bfbb6be283ba8c113584
Steven A. Zaluk <stevezaluk@gmail.com>
2025-06-08 18:28:28 -0400

    Implemented a custom middleware function for marshaling protobufs so that even empty fields can get returned

--- middleware/marshal.go
+++ middleware/marshal.go
@@ -1,3 +1,28 @@
 package middleware
 
+import (
+	"fmt"
+	"github.com/gofiber/fiber/v3"
+	"google.golang.org/protobuf/encoding/protojson"
+	"google.golang.org/protobuf/proto"
+)
 
+/*
+MarshalProtobuf - Marshal's and sends a protobuf while populating any errors that occur during the process. This
+function is required as by default protobuf emits empty fields
+*/
+func MarshalProtobuf(c fiber.Ctx, message proto.Message) error {
+	marshaler := protojson.MarshalOptions{
+		EmitUnpopulated: true,
+		UseProtoNames:   true,
+	}
+
+	data, err := marshaler.Marshal(message)
+	if err != nil {
+		wrappedErr := fmt.Errorf("%w (%v)", ErrFailedToBindResponse, err)
+		return BindError(c, wrappedErr)
+	}
+
+	c.Set("Content-Type", "application/json; charset=utf-8")
+	return c.Status(200).Send(data)
+}
