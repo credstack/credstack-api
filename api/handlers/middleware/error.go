b7e9bf7de6cb5e5221dfb17c526c4babe9d5edee
Steven A. Zaluk <stevezaluk@gmail.com>
2025-06-08 13:52:56 -0400

    Provided a function for marshaling errors into a JSON document

--- api/handlers/middleware/error.go
+++ api/handlers/middleware/error.go
@@ -1,3 +1,20 @@
 package middleware
 
+import (
+	"errors"
+	"github.com/gofiber/fiber/v3"
+	credstackErrors "github.com/stevezaluk/credstack-lib/errors" // this needs to be fixed
+)
 
+/*
+BindError - Takes a CredStack error and marshal's it into a JSON response
+*/
+func BindError(c fiber.Ctx, err error) error {
+	var casted credstackErrors.CredstackError
+
+	if !errors.As(err, &casted) {
+		return c.Status(500).JSON(fiber.Map{"error": err.Error()})
+	}
+
+	return c.Status(casted.HTTPStatusCode).JSON(fiber.Map{"errorCode": casted.Short(), "error": casted.Error()})
+}
