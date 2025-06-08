a004dccc6ca189edeba3df20404c5d0c619cd1b7
Steven A. Zaluk <stevezaluk@gmail.com>
2025-06-08 13:53:07 -0400

    Implemented support for fetching applications

--- api/handlers/management/application.go
+++ api/handlers/management/application.go
@@ -1,3 +1,26 @@
 package management
 
+import (
+	"github.com/gofiber/fiber/v3"
+	"github.com/stevezaluk/credstack-api/api"
+	"github.com/stevezaluk/credstack-api/api/handlers/middleware"
+	"github.com/stevezaluk/credstack-lib/application"
+)
 
+/*
+GetApplicationHandler - Provides a Fiber handler for processing a get request to /management/application. This should
+not be called directly, and should only ever be passed to Fiber
+
+TODO: Authentication handler needs to happen here
+TODO (Urgent): Logging here
+*/
+func GetApplicationHandler(c fiber.Ctx) error {
+	clientId := c.Query("client_id")
+
+	app, err := application.GetApplication(api.Server, clientId, true)
+	if err != nil {
+		return middleware.BindError(c, err)
+	}
+
+	return c.Status(200).JSON(&app)
+}
