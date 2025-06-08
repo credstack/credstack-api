402a9661386d686e3834761b9d2c6236f3d9a969
Steven A. Zaluk <stevezaluk@gmail.com>
2025-06-08 16:41:59 -0400

    Added a named error for when response binding fails

--- api/handlers/middleware/error.go
+++ api/handlers/middleware/error.go
@@ -6,6 +6,9 @@ import (
 	credstackErrors "github.com/stevezaluk/credstack-lib/errors" // this needs to be fixed
 )
 
+// ErrFailedToBindResponse - Provides a named error for when fiber can't bind a request body to a model
+var ErrFailedToBindResponse = credstackErrors.NewError(400, "RESP_BIND_FAILED", "http: Failed to bind request body to model")
+
 /*
 BindError - Takes a CredStack error and marshal's it into a JSON response
 */
@@ -13,8 +16,8 @@ func BindError(c fiber.Ctx, err error) error {
 	var casted credstackErrors.CredstackError
 
 	if !errors.As(err, &casted) {
-		return c.Status(500).JSON(fiber.Map{"error": err.Error()})
+		return c.Status(500).JSON(fiber.Map{"message": err.Error()})
 	}
 
-	return c.Status(casted.HTTPStatusCode).JSON(fiber.Map{"errorCode": casted.Short(), "error": casted.Error()})
+	return c.Status(casted.HTTPStatusCode).JSON(fiber.Map{"error": casted.Short(), "message": casted.Error()})
 }
