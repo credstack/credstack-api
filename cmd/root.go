3dc9fd4075311f3dabcdd515940ab1c38a4e4719
Steven A. Zaluk <stevezaluk@gmail.com>
2025-06-08 20:14:51 -0400

    Added proper routes for patching users

--- cmd/root.go
+++ cmd/root.go
@@ -38,7 +38,9 @@ var rootCmd = &cobra.Command{
 		api.App.Post("/management/application", management.PostApplicationHandler, middleware.LogMiddleware)
 		api.App.Patch("/management/application", management.PatchApplicationHandler, middleware.LogMiddleware)
 		api.App.Delete("/management/application", management.DeleteApplicationHandler, middleware.LogMiddleware)
+
 		api.App.Get("/management/user", management.GetUserHandler, middleware.LogMiddleware)
+		api.App.Patch("/management/user", management.PatchUserHandler, middleware.LogMiddleware)
 		api.App.Delete("/management/user", management.DeleteUserHandler, middleware.LogMiddleware)
 
 		err := api.Start(viper.GetInt("port"))
