b095ec3e9f24d3183d9fd8561d205defddd1f5ce
Steven A. Zaluk <stevezaluk@gmail.com>
2025-06-08 20:53:15 -0400

    Implemented support for fetching APIs

--- handlers/management/api.go
+++ handlers/management/api.go
@@ -1,1 +1,25 @@
 package management
+
+import (
+	"github.com/gofiber/fiber/v3"
+	server "github.com/stevezaluk/credstack-api/api"
+	"github.com/stevezaluk/credstack-api/middleware"
+	"github.com/stevezaluk/credstack-lib/api"
+)
+
+/*
+GetAPIHandler - Provides a Fiber handler for processing a GET request to /management/api. This should
+not be called directly, and should only ever be passed to Fiber
+
+TODO: Authentication handler needs to happen here
+*/
+func GetAPIHandler(c fiber.Ctx) error {
+	audience := c.Query("audience")
+
+	requestedApi, err := api.GetAPI(server.Server, audience)
+	if err != nil {
+		return middleware.BindError(c, err)
+	}
+
+	return middleware.MarshalProtobuf(c, requestedApi)
+}
